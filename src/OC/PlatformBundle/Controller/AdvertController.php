e0620a0329f51672993f09deb9a8a53eb1a051c8
ValentinClementFr <valentin.clement37@yahoo.fr>
2018-03-22 15:02:56 +0100

    Creation premiere variable twig

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller{
 	public function indexAction(){
-		$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig');
+		$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
 		return new Response($content);
 	}
 }
