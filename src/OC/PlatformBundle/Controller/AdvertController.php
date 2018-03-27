bf1faade6a87d3f9977a1a6113609f7782f530b9
ValentinClementFr <valentin.clement37@yahoo.fr>
2018-03-27 14:53:42 +0200

    modification AdvertController

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -1,20 +1,29 @@
-<?php 
+<?php
 
 // src/OC/PlatformBundle/Controller/AdvertController.php
 
 namespace OC\PlatformBundle\Controller;
 
-// N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdvertController extends Controller{
-	public function indexAction(){
-		$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
-		return new Response($content);
-	}
-	public function byeAction(){
-		$content = $this->get('templating')->render('OCPlatformBundle:Advert:bye.html.twig', array('nom' => 'claude'));
-		return new Response($content);
-	}
+class AdvertController extends Controller
+{
+  // La route fait appel à OCPlatformBundle:Advert:view,
+  // on doit donc définir la méthode viewAction.
+  // On donne à cette méthode l'argument $id, pour
+  // correspondre au paramètre {id} de la route
+  public function viewAction($id)
+  {
+    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+
+    // Ici, on récupèrera depuis la base de données
+    // l'annonce correspondant à l'id $id.
+    // Puis on passera l'annonce à la vue pour
+    // qu'elle puisse l'afficher
+
+    return new Response("Affichage de l'annonce d'id : ".$id);
+  }
+    
+  // ... et la méthode indexAction que nous avons déjà créée
 }
